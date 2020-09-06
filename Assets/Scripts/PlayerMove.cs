f9f17bb19df2e73c26da671e219658cec42cd40c
nick-moran <nmoran2@asu.edu>
2020-09-05 17:58:17 -0700

    basic player movement

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,58 +4,69 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public bool bobbing = true;
     public Rigidbody rb;
-    private Vector3 movement = new Vector3();
     public float speed = 10f;
-
     public Camera cam;
+    private Vector3 movement = new Vector3();
     private Vector3 height;
+    
+    public float dX = 2f;
+    public float sensitivity = 10f;
+    public float cycleAmt = 10f;
+    public float amplitude = 0.1f;
+    public float distFromPlayer = 2.29f;
+    private Vector3 rotateVector = new Vector3();
+    private float translateX;
     void Start()
     {
         rb =  GetComponent<Rigidbody>();
         rb.velocity = Vector3.zero;
         height = cam.transform.position;
+        rb.position = Vector3.zero;
     }
 
     void Update(){
-        movement = Vector3.zero;
+        translateX = Input.GetAxis("Mouse X") * dX;
+
+        rotateVector.Set(0, translateX, 0);
+        rotateVector*=sensitivity;
+
+        transform.Rotate(rotateVector);
+
+        movement = transform.rotation * move() * speed;
+        
+        if(bobbing && movement != Vector3.zero){
+            cam.transform.position = Vector3.Lerp(cam.transform.position,
+            new Vector3(cam.transform.position.x, distFromPlayer+amplitude*(Mathf.Sin(cycleAmt*Time.time)), cam.transform.position.z), Time.deltaTime*100);
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(transform.position + movement * Time.deltaTime);   
+    }
+
+    Vector3 move(){
+        Vector3 movement = Vector3.zero;
 
         if(Input.GetKey("w")){
             movement.z = 1;
-        } else {
-            // movement.z = 0;
         }
 
         if(Input.GetKey("a")){
             movement.x = -1;
-        } else {
-            // movement.x = 0;
         }
 
         if(Input.GetKey("d")){
             movement.x = 1;
-        } else {
-            // movement.x = 0;
         }
 
         if(Input.GetKey("s")){
             movement.z = -1;
-        } else {
-            // movement.z = 0;
-        }
-        movement = movement*speed;
-
-        if(movement != Vector3.zero){
-            cam.transform.position = Vector3.Lerp(cam.transform.position,
-            new Vector3(cam.transform.position.x, 5f+0.2f*(Mathf.Sin(5f*Time.time)), cam.transform.position.z), Time.deltaTime*100);
         }
 
-    }
-
-    // Update is called once per frame
-    void FixedUpdate()
-    {
-        rb.MovePosition(transform.position + movement * Time.deltaTime);   
+        return Vector3.Normalize(movement);
     }
 }
